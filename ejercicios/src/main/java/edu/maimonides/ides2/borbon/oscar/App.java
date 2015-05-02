cfff6fe7f24e68192ae29c0ae828d1ec90f97ec2
unknown <borbon.oscar@maimonides.edu>
2015-05-02 12:54:42 -0300

    primer commit

---
+++ ejercicios/src/main/java/edu/maimonides/ides2/borbon/oscar/App.java
@@ -0,0 +1,13 @@
+package edu.maimonides.ides2.borbon.oscar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ejercicios/src/main/java/edu/maimonides/ides2/borbon/oscar/Login.java
@@ -0,0 +1,5 @@
+package edu.maimonides.ides2.borbon.oscar;
+
+public class Login {
+
+}
