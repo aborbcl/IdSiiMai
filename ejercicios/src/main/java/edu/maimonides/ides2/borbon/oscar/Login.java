05e25e3d6f9ac69213804e0f7a40dd9597fd1040
unknown <borbon.oscar@maimonides.edu>
2015-05-02 13:20:16 -0300

    commit 2

--- ejercicios/src/main/java/edu/maimonides/ides2/borbon/oscar/Login.java
+++ ejercicios/src/main/java/edu/maimonides/ides2/borbon/oscar/Login.java
@@ -1,5 +1,10 @@
 package edu.maimonides.ides2.borbon.oscar;
 
 public class Login {
-
+	public boolean login(String username, String password){
+		if (username == password +"123"){
+			return true;
+		}
+		return false;
+	} 
 }
